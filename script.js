3237965d2fc34c532b8a1ff6708901379a6c9532
syedmaaz388 <63224539+syedmaaz388@users.noreply.github.com>
2023-04-27 15:39:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+function calculateTotalPrice() {
+      const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+      let totalPrice = 0;
+
+      priceElements.forEach((element) => {
+        const price = parseFloat(element.textContent);
+        totalPrice += price;
+      });
+
+      const grandTotalRow = document.createElement('tr');
+      const grandTotalCell = document.createElement('td');
+      grandTotalCell.setAttribute('colspan', '2');
+      grandTotalCell.setAttribute('data-ns-test', 'grandTotal');
+      grandTotalCell.textContent = 'Total Price: $' + totalPrice.toFixed(2);
+      grandTotalRow.appendChild(grandTotalCell);
+
+      const groceryTable = document.getElementById('groceryTable');
+      groceryTable.appendChild(grandTotalRow);
+    }
+
+    calculateTotalPrice();
